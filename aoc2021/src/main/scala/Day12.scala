5700e7d10a7443905b7caa6ff029ca2c40756d98
bnord <bnord01@gmail.com>
2021-12-12 19:50:13 +0100

    Day 12

---
+++ aoc2021/src/main/scala/Day12.scala
@@ -0,0 +1,53 @@
+import Utils.seqFromFileLines
+
+import scala.collection.mutable
+
+object Day12 extends App {
+  val input = seqFromFileLines("day12.txt")
+  val edges = input.map(_.split("-") match { case Array(a, b) => (a, b) })
+  type Node = String
+  val successors =
+    edges.foldLeft(Map[Node, Set[Node]]().withDefaultValue(Set())) {
+      case (succs, (a, b)) => succs + (a -> (succs(a) + b)) + (b -> (succs(b) + a))
+    }
+
+  def lower(s: Node) = !s.charAt(0).isUpper
+
+  val start = "start"
+  val end = "end"
+
+  // Part 1
+
+  def dfs_one(n: Node, seen: Set[Node] = Set()): Set[List[Node]] = {
+    if (seen(n))
+      Set()
+    else if (n == end)
+      Set(n :: Nil)
+    else {
+      val new_seen = if (lower(n)) seen + n else seen
+      successors(n).flatMap(dfs_one(_, new_seen)).map(n :: _)
+    }
+  }
+
+  println(dfs_one(start).size)
+
+
+  // Part 2
+
+  def dfs_two(n: Node, seen: Set[Node] = Set(), bonus: Boolean = true): Set[List[Node]] = {
+    if (seen(n)) {
+      if (!bonus || n == start || n == end)
+        Set()
+      else
+        successors(n).flatMap(dfs_two(_, seen, bonus = false)).map(n :: _)
+    }
+    else if (n == end)
+      Set(n :: Nil)
+    else {
+      val new_seen = if (lower(n)) seen + n else seen
+      successors(n).flatMap(dfs_two(_, new_seen, bonus)).map(n :: _)
+    }
+  }
+
+  println(dfs_two(start).size)
+}
