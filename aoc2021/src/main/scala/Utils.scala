3ac0bcbea2186eb522bda31698edef1277863286
bnord <bnord01@gmail.com>
2021-12-22 12:55:53 +0100

    Timing util

--- aoc2021/src/main/scala/Utils.scala
+++ aoc2021/src/main/scala/Utils.scala
@@ -3,4 +3,12 @@ import scala.io.Source
 object Utils {
   def seqFromFileLines(fileName: String): Seq[String] =
     Source.fromResource(fileName).getLines().toSeq
+
+  def time[R](body: => R): R = {
+    val start = System.currentTimeMillis()
+    val result = body
+    val end = System.currentTimeMillis()
+    println(f"Elapsed time: ${end - start}ms")
+    result
+  }
 }
