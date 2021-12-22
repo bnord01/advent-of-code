4fb4793c368766699e62aba0972407264eb1403d
bnord <bnord01@gmail.com>
2021-12-22 12:55:40 +0100

    Day 21

--- aoc2021/src/main/scala/Day21.scala
+++ aoc2021/src/main/scala/Day21.scala
@@ -28,30 +28,14 @@ object Day21 extends App {
 
   val rolls = Array.from(for (i1 <- 1 to 3; i2 <- 1 to 3; i3 <- 1 to 3) yield i1 + i2 + i3)
 
-  // Sadly the naive approach chokes while hashing
-  val wins: mutable.Map[(Int, Int, Int, Int), (Long, Long)] = mutable.Map.empty.withDefault { case (s1, s2, p1, p2) =>
+  val win_table: mutable.Map[(Int, Int, Int, Int), (Long, Long)] = mutable.Map.empty
+
+  def wins(s1: Int, s2: Int, p1: Int = 0, p2: Int = 0): (Long, Long) = win_table.getOrElseUpdate((s1, s2, p1, p2), {
     if (p1 > 20) (1, 0)
     else if (p2 > 20) (0, 1)
     else rolls.map(d => wins(s2, (s1 + d) % 10, p2, p1 + 1 + ((s1 + d) % 10))).foldLeft((0L, 0L)) { case ((x1, x2), (y2, y1)) => (x1 + y1, x2 + y2) }
-  }
+  })
 
-  // Use arrays instead, infinitely faster
-  val win1_table = Array.ofDim[Long](10, 10, 21, 21)
-  val win2_table = Array.ofDim[Long](10, 10, 21, 21)
-  val done_table = Array.ofDim[Boolean](10, 10, 21, 21)
-
-  def wins2(s1: Int, s2: Int, p1: Int = 0, p2: Int = 0): (Long, Long) = {
-    if (p1 > 20) (1, 0)
-    else if (p2 > 20) (0, 1)
-    else if (done_table(s1)(s2)(p1)(p2)) (win1_table(s1)(s2)(p1)(p2), win2_table(s1)(s2)(p1)(p2))
-    else {
-      val r = rolls.map(d => wins2(s2, (s1 + d) % 10, p2, p1 + 1 + ((s1 + d) % 10))).foldLeft((0L, 0L)) { case ((x1, x2), (y2, y1)) => (x1 + y1, x2 + y2) }
-      done_table(s1)(s2)(p1)(p2) = true; win1_table(s1)(s2)(p1)(p2) = r._1; win2_table(s1)(s2)(p1)(p2) = r._2
-      r
-    }
-  }
-
-  // println(wins2(4-1,8-1)) // Test
-
-  println(wins2(9 - 1, 3 - 1) match { case (a, b) => math.max(a, b) }) // Real
+  // println(wins(4-1,8-1)) // Test
+  println(wins(9 - 1, 3 - 1) match { case (a, b) => math.max(a, b) }) // Real
 }
